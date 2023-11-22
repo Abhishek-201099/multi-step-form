63906a5f0074114c2e92e8a4e05235f48c6e00eb
Abhishek Bhattacharjee <ahishek1250@gmail.com>
2023-11-22 19:48:51 +0530

    feat: Added pageNotFound page and added styling for it

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,3 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function PageNotFound() {
-  return <div>Page not Found</div>;
+  return (
+    <div className="page-not-found">
+      <div>
+        <p className="notfound-text">🚫 Page not Found</p>
+        <Link to="/" replace>
+          &larr; Go back
+        </Link>
+      </div>
+    </div>
+  );
 }
